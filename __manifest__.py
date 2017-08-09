4348168609b122541e0982220ee7768a94583092
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-08-08 20:16:19 -0700

    Fixed Issues w/ import

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
 This custom module for Odoo Online automatically creates an account.payment
 record on successful payment transactions (from payment acquirer). For Authorize.net ONLY.
         """,
-    'depends': ['base','payment_authorize','account_accountant'],
+    'depends': ['base','payment_authorize','account_accountant','base_action_rule'],
     'data': [
         'data/fields_actions.xml',
         'data/views.xml',
