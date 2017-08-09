729fa2d40bdab879eaabcaca722e85f9139832e6
JoseMCoronado <j.coronado.personal@gmail.com>
2017-08-08 19:37:46 -0700

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Made for Odoo Online. See Odoo LICENSE file for full copyright and licensing details.
+
+{
+    'name': '[JOS] Automated Payment on Successful Payment TX',
+    'category': 'Sale',
+    'summary': 'Custom',
+    'version': '1.0',
+    'description': """
+This custom module for Odoo Online automatically creates an account.payment
+record on successful payment transactions (from payment acquirer). For Authorize.net ONLY.
+        """,
+    'depends': ['base','payment_authorize','account_accountant'],
+    'data': [
+        'data/views.xml',
+        'data/fields_actions.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
